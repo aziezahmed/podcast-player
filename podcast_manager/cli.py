b4229209cd5b87accb2286ad1dfa131fdff612c7
Aziez Ahmed Chawdhary <aziezahmed@gmail.com>
2017-10-05 07:22:16 +0100

    Adding the commands folder with a hello command

--- podcast_manager/cli.py
+++ podcast_manager/cli.py
@@ -3,6 +3,7 @@ podcast
 
 Usage:
   podcast list
+  podcast hello
   podcast add <url>
   podcast -h | --help
   podcast --version
@@ -12,12 +13,12 @@ Options:
   --version                         Show version.
 
 Examples:
-  podcast
+  podcast list
   podcast add <url>
 
 Help:
   For help using this tool, please open an issue on the Github repository:
-  https://github.com/rdegges/skele-cli
+  https://github.com/aziezahmed/podcast-manager
 """
 
 from inspect import getmembers, isclass
@@ -28,11 +29,23 @@ from . import __version__ as VERSION
 from podcast_manager.datastore import DataStore
 from podcast_manager.feed import Feed
 
+
 def main():
-
+    import podcast_manager.commands
+    
     dataStore = DataStore()
-
+    
+    import podcast_manager.commands
+    
     options = docopt(__doc__, version=VERSION)
+
+    for (k, v) in options.items(): 
+        if hasattr(podcast_manager.commands, k) and v:
+            module = getattr(podcast_manager.commands, k)
+            podcast_manager.commands = getmembers(module, isclass)
+            command = [command[1] for command in podcast_manager.commands if command[0] != 'Base'][0]
+            command = command(options)
+            command.run()
 
     if (options["add"]):
         dataStore.add_podcast(options["<url>"])
