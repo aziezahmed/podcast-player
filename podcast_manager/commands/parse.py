6e21382f94d6eaf2a4012ebaecd897618e3d3b84
Aziez Ahmed Chawdhary <aziezahmed@gmail.com>
2017-10-06 22:14:38 +0100

    only fetch the selected podcast url

--- podcast_manager/commands/parse.py
+++ podcast_manager/commands/parse.py
@@ -12,16 +12,18 @@ class Parse(Base):
     def run(self):
         dataStore = DataStore()
         podcasts = dataStore.get_podcasts()
-        data = []
-        for podcast in podcasts:
-            feed = feedparser.parse(podcast)
-            data.append(feed)
-        for index, feed in enumerate(data):
-            print(str(index+1) + " - " + feed.feed.title)
+        podcast_names = dataStore.get_podcast_names()
+        
+        for index, name in enumerate(podcast_names):
+            print(str(index+1) + " - " + name)
+        
         choice = input('Which feed: ')
-        feed = data[int(choice)-1]
+        
+        feed = feedparser.parse(podcasts[int(choice)-1]) 
         feed.entries.reverse()
+        
         for index, entry in enumerate(feed.entries):
             print(str(index+1) + " - " + entry['title'])
+        
         choice = input('Which episode: ')
         print(feed.entries[int(choice)-1]["link"])
