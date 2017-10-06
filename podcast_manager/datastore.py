b0ed39cbc1778514cea455da1eed9dc4f69a663a
Aziez Ahmed Chawdhary <aziezahmed@gmail.com>
2017-10-06 21:37:28 +0100

    Using sqlite database to store podcasts instead of text file

--- podcast_manager/datastore.py
+++ podcast_manager/datastore.py
@@ -1,32 +1,33 @@
 import os
 import sys
 from os.path import expanduser
+from sqlobject import *
+import feedparser
+
+class PodcastDatabase(SQLObject):
+    name = StringCol()
+    url = StringCol()
 
 class DataStore():
     def __init__(self):
+        
         self.basedir = "~/.podcast"
-        if not os.path.exists(expanduser(self.basedir)):
-            print("Creating base dir %s"%self.basedir)
-            os.makedirs(expanduser(self.basedir))
-        subscriptions = expanduser(self.basedir) + os.sep + 'subscriptions'
-        if not os.path.exists(subscriptions):
-            print("Creating empty subscriptions file %s"%subscriptions)
-            open(subscriptions,'a').write("# Add your RSS/ATOM subscriptions here.\n\n")
-        subscribed_feeds = []
-        with open(subscriptions, 'r') as f:
-            for line in f:
-                feed = line.strip()
-                if feed.startswith("#") or len(feed) == 0:
-                    continue
-                subscribed_feeds.append(feed)
-        self.podcasts = subscribed_feeds
+            
+        PodcastDatabase._connection = sqlite.builder()(expanduser(self.basedir + os.sep + 'podcast.sqlite'), debug=False)
+        PodcastDatabase.createTable(ifNotExists=True)
+        
+        podcasts = list(PodcastDatabase.select())
+        podcast_urls = []
+        
+        for podcast in podcasts:
+            podcast_urls.append(podcast.url)
+        
+        self.podcasts = podcast_urls
 
     def get_podcasts(self):
         return self.podcasts
 
     def add_podcast(self, podcast_url):
         self.podcasts.append(podcast_url)
-
-        subscriptions = expanduser(self.basedir) + os.sep + 'subscriptions'
-        with open(subscriptions, 'a') as f:
-            f.write(podcast_url + "\n")
+        feed = feedparser.parse(podcast_url)
+        PodcastDatabase(name=feed.feed.title, url=podcast_url)
