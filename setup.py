8cac7f080e66d84b9c952fd7d7aca0b9c4b6b8d8
Aziez Ahmed Chawdhary <aziezahmed@gmail.com>
2017-10-08 11:47:40 +0100

    adding install requirements

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     ],
     keywords = 'cli',
     packages = find_packages(exclude=['docs', 'tests*']),
-    install_requires = ['docopt'],
+    install_requires = ['docopt', 'feedparser', 'sqlobject'],
     extras_require = {
         'test': ['coverage', 'pytest', 'pytest-cov'],
     },
