d81549f64b64652f2955ac5cb86268dbb3421ed7
Aziez Ahmed Chawdhary <aziezahmed@gmail.com>
2017-10-18 16:09:37 +0100

    Remove unnecessary import

--- podcast_player/podcast_database.py
+++ podcast_player/podcast_database.py
@@ -1,7 +1,6 @@
 """An SQL Object to store information about our podcast library. """
 
 from sqlobject import *
-import feedparser
 
 class PodcastDatabase(SQLObject):
     """A database of podcast names and urls."""
