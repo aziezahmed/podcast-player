ee7201bd071299ca666e95c3b15458efbdc1cae6
Aziez Ahmed Chawdhary <aziezahmed@gmail.com>
2017-10-24 21:18:36 +0100

    Setting the default player to MPV in no-video mode

--- podcast_player/user_settings.py
+++ podcast_player/user_settings.py
@@ -15,7 +15,7 @@ class UserSettings(object):
 
         self.config = configparser.ConfigParser()
         self.config.add_section('podcast')
-        self.config['podcast']['player'] = "mplayer"
+        self.config['podcast']['player'] = "mpv --no-video"
 
         if not os.path.isfile(self.user_config_path):
             with open(self.user_config_path, 'w') as f:
