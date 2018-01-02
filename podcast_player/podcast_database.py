adcfc2ea46aedf607a962d40884d8bd1647620dd
Aziez Ahmed Chawdhary <aziezahmed@gmail.com>
2018-01-02 14:48:52 +0000

    Delete podcast_database.py

--- podcast_player/podcast_database.py
+++
@@ -1,8 +0,0 @@
-"""An SQL Object to store information about our podcast library. """
-
-from sqlobject import *
-
-class PodcastDatabase(SQLObject):
-    """A database of podcast names and urls."""
-    name = StringCol()
-    url = StringCol()
