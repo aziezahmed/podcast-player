535cd989ad9f4b926d6f6469f684b455443d87af
Aziez Ahmed Chawdhary <aziezahmed@gmail.com>
2022-04-21 12:37:34 +0100

    chore(test) fix cli test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,20 +4,20 @@
 from subprocess import PIPE, Popen as popen
 from unittest import TestCase
 
-from skele import __version__ as VERSION
+from podcast_player import __version__ as VERSION
 
 class TestHelp(TestCase):
     def test_returns_usage_information(self):
-        output = popen(['skele', '-h'], stdout=PIPE).communicate()[0]
+        output = popen(['podcast', '-h'], stdout=PIPE).communicate()[0]
         self.assertTrue('Usage:'.encode('utf-8') in output)
 
-        output = popen(['skele', '--help'], stdout=PIPE).communicate()[0]
+        output = popen(['podcast', '--help'], stdout=PIPE).communicate()[0]
         self.assertTrue('Usage:'.encode('utf-8') in output)
 
 
 class TestVersion(TestCase):
     def test_returns_version_information(self):
-        output = popen(['skele', '--version'], stdout=PIPE).communicate()[0]
+        output = popen(['podcast', '--version'], stdout=PIPE).communicate()[0]
         self.assertEqual(output.strip(), VERSION.encode('utf-8'))
 
 
